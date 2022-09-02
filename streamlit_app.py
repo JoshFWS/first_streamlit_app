808880a6533dc60ebe5a8af0285667d030033f3f
JoshFWS <108554807+JoshFWS@users.noreply.github.com>
2022-09-02 08:48:20 -0500

    Update streamlit_app.py
    Added a banner with BYO Smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣 Omega 3 & Berry Oatmeal')
 streamlit.text('🥗 Kale, Spinach, & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Eggs')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
