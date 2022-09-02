afa79107f2afcbf98a765ede6f3b4da54f6df1a0
JoshFWS <108554807+JoshFWS@users.noreply.github.com>
2022-09-02 08:44:10 -0500

    Update streamlit_app.py
    Added Header for Menu name and 3 items to the menu. Corrected spelling of smoothie and pluralized egg

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Berry Oatmeal')
-streamlit.text('Kale, Spinach, & Rocket Rommthie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Kale, Spinach, & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Eggs')
